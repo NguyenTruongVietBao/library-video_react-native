f6b0c2a4b9a7a1d5e482d5f9ddeaa5a8e3d5f969
NguyenTruongVietBao <nguyentvbao2403@gmail.com>
2024-10-30 23:45:30 +0700

    done onboarding

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,39 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 
 export default function Page() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Library Video</Text>
-      <StatusBar style="auto" />
-      <Link href="/home">Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              <Text className="text-secondary-200"> Viet Bao</Text> Cute
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute-bottom -top-1 -right-2"
+              resizeMethod="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Save your memory here !
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
