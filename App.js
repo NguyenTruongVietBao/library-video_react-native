896102c036850ed4359e0e35414b229d54bcf15f
NguyenTruongVietBao <nguyentvbao2403@gmail.com>
2024-11-01 15:16:33 +0700

    update auth view

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
-import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-
-export default function App() {
-  const [name, setName] = useState("");
-  return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  textInput: {
-    borderWidth: 1,
-    width: 200,
-  },
-});
+// import { useState } from "react";
+// import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+//
+// export default function App() {
+//   const [name, setName] = useState("");
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello!</Text>
+//     </View>
+//   );
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   textInput: {
+//     borderWidth: 1,
+//     width: 200,
+//   },
+// });
